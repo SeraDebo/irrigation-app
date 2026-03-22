b719f35ce2f7738beead93dfd13cd79b0bfd34a4
Debajyoti Paul <debajyotipaul.work123@gmail.com>
2026-03-23 04:36:59 +0530

    fix of db

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from supabase import create_client
 
-
-url = st.secrets["https://cpwtjtevrkqfpldaqess.supabase.co"]
-key = st.secrets["sb_publishable_s4QameDnJax6B-ZOohxG2g_Qc4xpW04"]
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
