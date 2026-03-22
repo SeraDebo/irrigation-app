9c550c9faebca01ac30013933d2816de5ad78b93
Debajyoti Paul <debajyotipaul.work123@gmail.com>
2026-03-23 04:24:29 +0530

    fix requuirements.txt and db.py

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
+import streamlit as st
 from supabase import create_client
 
-url = "https://cpwtjtevrkqfpldaqess.supabase.co"
-key = "sb_publishable_s4QameDnJax6B-ZOohxG2g_Qc4xpW04"
+
+url = st.secrets["https://cpwtjtevrkqfpldaqess.supabase.co"]
+key = st.secrets["sb_publishable_s4QameDnJax6B-ZOohxG2g_Qc4xpW04"]
 
 supabase = create_client(url, key)
